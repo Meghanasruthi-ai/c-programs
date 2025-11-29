b06a9ee38180198a0ecca9c9076f9984f1450a01
Meghanasruthi-ai <sruthitmegana@gmail.com>
2025-11-29 10:24:01 +0530

    Create datatypes.c

---
+++ datatypes.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 20;
+
+    float height = 5.2;
+
+    double pi = 3.14159;
+    
+    char grade = 'A';
+
+    printf("Age: %d\n", age);
+    printf("Height: %.1f\n", height);
+    printf("Pi: %.5lf\n", pi);
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}
