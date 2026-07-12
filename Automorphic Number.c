376b551ee6103f051735a21324f3be6103d8adf4
Meghanasruthi-ai <sruthitmegana@gmail.com>
2026-07-13 02:20:13 +0530

    Add Automorphic Number program in C

---
+++ Automorphic Number.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, square, temp;
+
+    printf("Enter Number: ");
+    scanf("%d", &n);
+
+    square = n * n;
+    temp = n;
+
+    while(temp > 0)
+    {
+        if(temp % 10 != square % 10)
+        {
+            printf("Not an Automorphic Number");
+            return 0;
+        }
+
+        temp /= 10;
+        square /= 10;
+    }
+
+    printf("Automorphic Number");
+
+    return 0;
+}
